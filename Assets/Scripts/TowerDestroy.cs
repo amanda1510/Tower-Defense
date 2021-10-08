734b096570bae424188a6e90792e897e62e62517
Amanda Divia Ardianti <amandadivia4@gmail.com>
2021-10-08 15:21:39 +0700

    Latihan 10 Tower Defense

---
+++ Assets/Scripts/TowerDestroy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerDestroy : MonoBehaviour
+{
+    public bool isPlayer = true;
+
+    private void OnDestroy()
+    {
+        if (!Data.isGameOver)
+            if (isPlayer)
+            {
+                Data.isGameOver = true;
+                Data.isComplete = false;
+                Debug.Log("Player Lost");
+            }
+            else
+            {
+                Data.isGameOver = true;
+                Data.isComplete = true;
+                Debug.Log("Player Win");
+            }
+ 
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
